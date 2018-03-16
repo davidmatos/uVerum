709eba7ae1c21719873c649fce2b7a1bf00ab163
David Matos <david.matos21@gmail.com>
2018-03-16 18:05:15 +0000

    Added microVerum Bubble initial project

---
+++ microVerumBubble/src/main/java/pt/inescid/microVerum/Bubble/MicroVerumBubbleApplication.java
@@ -0,0 +1,12 @@
+package pt.inescid.microVerum.Bubble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroVerumBubbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroVerumBubbleApplication.class, args);
+	}
+}
